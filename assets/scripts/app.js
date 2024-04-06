b84cab5b90c0b806a6f419b45b3c705f37ead813
Omm1s <165832528+Omm1s@users.noreply.github.com>
2024-04-06 19:25:57 +0200

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+function sabiranje(broj1,broj2){
+    let zbir=broj1+broj2;
+    return zbir;
+}
+console.log(sabiranje(2,3));
+
+function oduzmi(broj1,broj2){
+    let razlika=broj1-broj2;
+    return razlika;
+}
+console.log(oduzmi(8,2));
+
+function pomnozi(broj1,broj2){
+    let proizvod=broj1*broj2;
+    return proizvod;
+}
+console.log(pomnozi(5,7));
+
+function podijeli(broj1,broj2){
+    let kolicnik=broj1/broj2;
+    return kolicnik;
+}
+console.log(podijeli(10,4));
